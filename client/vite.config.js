24ef3100b57ec4e2b710a304a12313158eec60e8
Shital Jha <157676732+shitaljha14@users.noreply.github.com>
2025-06-19 11:44:08 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/BloodLink/', // must match your GitHub repo name
   plugins: [react()],
-})
-
+});
